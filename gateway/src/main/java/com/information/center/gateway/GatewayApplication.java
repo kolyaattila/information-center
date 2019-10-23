15d3c19bcd9b3872a9cff9ff3e25b434b258bbb5
kolyaattila <37974930+kolyaattila@users.noreply.github.com>
2019-10-23 21:12:41 +0300

    initial commit

---
+++ gateway/src/main/java/com/information/center/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.information.center.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
---
+++ topic-service/src/test/java/com/information/center/topicservice/TopicServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.information.center.topicservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
