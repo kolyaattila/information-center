40ec90d87d1baef81dcf06e63d4fb90b230fe515
Attila <attila.kolya@yahoo.com>
2020-04-12 17:20:54 +0300

    IC-24: Convert the book to sql

--- question-service/src/main/resources/db/data/1.0/question.sql
+++ question-service/src/main/resources/db/data/1.0/question.sql
@@ -1,9 +1,13 @@
 create table public.Question (
     id                bigserial constraint question_pkey primary key,
-    name              varchar(255) not null,
-    created           timestamp,
-    external_id       varchar(255) not null unique,
-    topic_id          varchar(255) not null,
-    difficulty        varchar(10) not null,
+    name              varchar(1000) not null,
+    created           timestamp default CURRENT_DATE,
+    external_id       varchar(50) not null unique,
+    topic_id          varchar(30),
+    difficulty        varchar(10) not null default 'EASY',
+    chapter           varchar(100),
+    verified          boolean not null default 'false',
+    book              varchar(100),
+    question_number   int,
     CONSTRAINT question_check_difficulty CHECK (difficulty in ('EASY','HARD','MEDIUM'))
 );
